d9e46f532ddd48e3c403744f8e3c02411637b453
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-04-04 15:34:06 +0200

    +

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,67 +1,40 @@
-import { Component } from "react";
 import CommentsList from "./CommentsList";
 import AddComment from "./AddComment";
+import { useEffect, useState } from "react";
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhYzMxMjY4MzQzMTAwMTRkZWE3NjEiLCJpYXQiOjE2ODA1MjQwNTAsImV4cCI6MTY4MTczMzY1MH0.LP8fehJyM-iPCgOwm4Qa_PyYUIVCO9giwhA8P8ogogQ";
 
-class CommentArea extends Component {
-  state = {
-    comments: []
-  };
+const CommentArea = props => {
+  const [comments, setComments] = useState([]);
 
-  // componentDidMount ora avverrà al primo montaggio del componente, cioè dopo la prima selezione di una card nella lista
-  componentDidMount() {
-    console.log("didMount()");
-
-    this.fetchComments();
-  }
-
-  // fetchComments viene chiamato in: componentDidMount, componentDidUpdate e anche dopo la post interna ad AddComment
-  fetchComments = async () => {
+  const fetchComments = async () => {
     try {
-      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + props.asin, {
         headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhZThlNTY4MzQzMTAwMTRkZWE3ZWYiLCJpYXQiOjE2ODA1MzM3MzMsImV4cCI6MTY4MTc0MzMzM30.BybWrL_zO0q23jXsrG8pA-yPWXt9QYBf4zZcRNL920U"
-        }
+          Authorization: token,
+        },
       });
 
       if (response.ok) {
         const commentsArr = await response.json();
-        console.log("data retrieved, setState imminent....");
-        // setState  accetta una callback come secondo parametro opzionale, che verrà chiamata dopo che lo stato è stato effettivamente aggiornato
-        this.setState({ comments: commentsArr }, () => console.log("setState", this.state.comments));
+
+        setComments(commentsArr);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("didUpdate()");
+  useEffect(() => {
+    fetchComments();
+  }, [props.asin]);
 
-    // controllo di guardia per rifare la fetch SOLO SE cambia la prop, non quando c'è un aggiornamento di stato
-    if (prevProps.asin !== this.props.asin) {
-      // in questo modo la lista dei commenti cambierà nel momento in cui selezioneremo un altra card, perché l'asin sarà cambiato dopo la nuova selezione
-      this.fetchComments();
-    } else {
-      console.log("componentDidUpdate but NO FETCH!");
-    }
-  }
-
-  componentWillUnmount() {
-    console.log("willUnmount()");
-  }
-
-  render() {
-    console.log("render()");
-    return (
-      <div>
-        {/* la prop fetchComments dà la possibilità ad AddComment di rifare la fetch e ottenere la lista di commenti aggiornati 
-         che servirà poi a CommentList qua sotto per ricevere la nuova lista aggiornata, con anche l'ultimo appena inserito */}
-        <AddComment asin={this.props.asin} fetchComments={this.fetchComments} />
-        <CommentsList comments={this.state.comments} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <AddComment asin={props.asin} fetchComments={fetchComments} />
+      <CommentsList comments={comments} />
+    </div>
+  );
+};
 
 export default CommentArea;
